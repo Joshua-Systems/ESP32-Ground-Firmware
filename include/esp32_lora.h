78e70f3d44e05447ce677352b498140e72e1e3da
Josh <eejl609@aucklanduni.ac.nz>
2025-08-20 11:27:57 +1200

    Basic Main.cpp Set up, needs to be modularly tested

--- include/esp32_lora.h
+++ include/esp32_lora.h
@@ -1,9 +1,6 @@
 #include <StreamString.h>
 #include <WString.h>
-#ifndef LORA_H
 #define LORA_H
 void initLoRa();                            // Call once in setup
 void sendLoRaMessage(String message);       // Send message
 String receiveLoRaMessage(String &message); // Receive message, if available
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <LoRa.h>
+#include <SPI.h>
+#include <String.h>
+#include <StreamString.h>
+#include <WString.h>
+#include "esp32_lora.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  initLoRa(); //
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  sendLoRaMessage("Hello, LoRa!");
+  String receivedMessage;
+  String result = receiveLoRaMessage(receivedMessage);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
